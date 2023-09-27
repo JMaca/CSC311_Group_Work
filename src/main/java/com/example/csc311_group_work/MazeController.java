01959888b58ed4c6fb31d65626b1064ce123161a
JMaca <70349528+JMaca@users.noreply.github.com>
2023-09-27 19:24:54 -0400

    Added a movement method for the Image to be moved with keystrokes of up,left,right, and down.

--- src/main/java/com/example/csc311_group_work/MazeController.java
+++ src/main/java/com/example/csc311_group_work/MazeController.java
@@ -1,18 +1,35 @@
 package com.example.csc311_group_work;
 
 import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-public class MazeController {
-    @FXML
-    public ImageView robotImg;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public void movementCheck(KeyEvent e){
-        if (e.getCode() == KeyCode.UP) {
-            robotImg.setLayoutY(robotImg.getLayoutY() + 2);
-        }
-}
+public class MazeController implements Initializable {
+    @FXML
+    public ImageView robotImg, mazeImg;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mazeImg.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent key) {
+                if (key.getCode().equals(KeyCode.RIGHT)) {
+                    robotImg.setLayoutX(robotImg.getLayoutX() + 5);
+                } else if (key.getCode().equals(KeyCode.LEFT)) {
+                    robotImg.setLayoutX(robotImg.getLayoutX() - 5);
+                }else if (key.getCode().equals(KeyCode.UP)) {
+                    robotImg.setLayoutY(robotImg.getLayoutY() - 5);
+                }else if (key.getCode().equals(KeyCode.DOWN)) {
+                    robotImg.setLayoutY(robotImg.getLayoutY() + 5);
+                }
+            }
+        });
+    }
 }
