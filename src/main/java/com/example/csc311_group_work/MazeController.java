22d0439305ac6980282af233d9c596adfa3d0a4e
catamv3 <catamv3@farmingdale.edu>
2023-09-30 13:13:28 -0400

    tried to mimmick the code that moves the robot to move the car, but im having trouble getting it to work

--- src/main/java/com/example/csc311_group_work/MazeController.java
+++ src/main/java/com/example/csc311_group_work/MazeController.java
@@ -1,11 +1,8 @@
 package com.example.csc311_group_work;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
@@ -17,37 +14,27 @@ import javafx.scene.paint.Color;
 import javafx.scene.layout.StackPane;
 import javafx.scene.SnapshotParameters;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class MazeController  {
+public class MazeController{
 
     @FXML
-    private ImageView mazeImg, maze2Image;
+    private ImageView mazeImg, maze2Img;
     @FXML
-    private StackPane robotImgContainer;
+    private StackPane robotImgContainer, carImgContainer;
     private static final double MOVE_SIZE = 3.0; //Change this to change speed of robot
 
-    @FXML
-    private Stage stage;
-
+    @FXML private Stage stage;
     @FXML
     private Scene scene;
 
     @FXML
     private Parent root;
 
+    @FXML
+    private MenuItem switch1;
 
-
-    /**
-     * event handler for the switch MenuItem under file.
-     * this will switch to the initial maze.
-     * @param event
-     * @throws IOException
-     */
     @FXML
     public void switchToMaze1(ActionEvent event) throws IOException {
         stage = new Stage();
@@ -73,13 +60,141 @@ public class MazeController  {
         stage.show();
     }
 
-    /*
 
-     */
+    //for the stackpane to move, link the imageView of the maze with the handleKeyMvmt
     /**
      *  Handles the movement of the robot based on what key is being pressed
      * @param event when arrow key is pressed
      */
+    @FXML
+    public void handleCarMovement(KeyEvent event){
+        //PixelReader will get the pixel colors
+        WritableImage writableImage = maze2Img.snapshot(new SnapshotParameters(), null);
+        PixelReader pixelReader = writableImage.getPixelReader();
+
+        switch (event.getCode()){
+            case UP:
+                if (carCanMoveUp(carImgContainer.getLayoutX(), carImgContainer.getLayoutY(), pixelReader)){
+                    moveCar(0, -MOVE_SIZE);
+                }
+                break;
+            case DOWN:
+                if (carCanMoveDown(carImgContainer.getLayoutX(), carImgContainer.getLayoutY(), pixelReader)){
+                    moveCar(0, MOVE_SIZE);
+                }
+                break;
+            case LEFT:
+                if (carCanMoveLeft(carImgContainer.getLayoutX(), carImgContainer.getLayoutY(), pixelReader)){
+                    moveCar(-MOVE_SIZE, 0);
+                }
+                break;
+            case RIGHT:
+                if (carCanMoveRight(carImgContainer.getLayoutX(), carImgContainer.getLayoutY(), pixelReader)){
+                    moveCar(MOVE_SIZE, 0);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private boolean isMaze2w(Color color){
+        // RGB values for blue wall
+        return color.equals(Color.web("#076eff"));
+    }
+
+    /**
+     * Checks to see if the robot can move left without hitting a wall
+     * @param x x location
+     * @param y y location
+     * @param pixelReader used to read the pixel color from mazeImg
+     * @return returns true or false based on if the robot can move left
+     */
+    private boolean carCanMoveLeft(double x, double y, PixelReader pixelReader){
+        //This for loop checks the line of pixels to the left of the robot
+        for(int i = 0; i < carImgContainer.getPrefHeight(); i++) {
+            Color color = pixelReader.getColor((int) x - 1, (int) y + i);
+            if(isMaze2w(color)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks to see if the robot can move right without hitting a wall
+     * @param x x location
+     * @param y y location
+     * @param pixelReader used to read the pixel color from mazeImg
+     * @return returns true or false based on if the robot can move right
+     */
+    private boolean carCanMoveRight(double x, double y, PixelReader pixelReader){
+        for (int i = 0; i < carImgContainer.getPrefHeight(); i++){
+            Color color = pixelReader.getColor((int) (x + carImgContainer.getPrefWidth() + 1), (int) y + i);
+            if (isMaze2w(color)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks to see if the robot can move up without hitting a wall
+     * @param x x location
+     * @param y y location
+     * @param pixelReader used to read the pixel color from mazeImg
+     * @return returns true or false based on if the robot can move up
+     */
+    private boolean carCanMoveUp(double x, double y, PixelReader pixelReader){
+        for (int i = 0; i < carImgContainer.getPrefWidth(); i++) {
+            Color color = pixelReader.getColor((int) x + i, (int) y - 1);
+            if (isMaze2w(color)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks to see if the robot can move down without hitting a wall
+     * @param x x location
+     * @param y y location
+     * @param pixelReader used to read the pixel color from mazeImg
+     * @return returns true or false based on if the robot can move down
+     */
+    private boolean carCanMoveDown(double x, double y, PixelReader pixelReader){
+        for (int i = 0; i < carImgContainer.getPrefWidth(); i++){
+            Color color = pixelReader.getColor((int) x + i, (int) (y + carImgContainer.getPrefHeight() + 1));
+            if (isMaze2w(color)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Moves the robots position
+     * @param dx change in x direction
+     * @param dy change in y direction
+     */
+    private void moveCar(double dx, double dy){
+        carImgContainer.setLayoutX(carImgContainer.getLayoutX() + dx);
+        carImgContainer.setLayoutY(carImgContainer.getLayoutY() + dy);
+    }
+
+
+
+
+
+
+
+
+
+    /**
+     *  Handles the movement of the robot based on what key is being pressed
+     * @param event when arrow key is pressed
+     */
+    @FXML
     public void handleKeyMovement(KeyEvent event){
         //PixelReader will get the pixel colors
         WritableImage writableImage = mazeImg.snapshot(new SnapshotParameters(), null);
